79b44fccb8f6390c3098cb625dd016569a587645
Nathachai Thongniran <inid3a@gmail.com>
2020-10-29 19:00:43 +0700

    :pencil: (badge) remove "Visits Badge" badge

--- event.py
+++ event.py
@@ -81,7 +81,7 @@ clock_percent_d = {
 
 print("""Hi :smiley: :wave:
 
-[![Visits Badge](https://badges.pufler.dev/visits/jojoee/jojoee)](https://github.com/jojoee/jojoee)<img src="https://jojoee.jojoee.com/api/utcnow" width="120" height="20">
+<img src="https://jojoee.jojoee.com/api/utcnow" width="120" height="20">
 
 In the latest %s commits :bug:, am I morning person ? 
 | | | | |%%|

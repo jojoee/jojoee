a9e6ab98fa5f2de8fc6f6888d3dfcdcd2bb6aa2c
Nathachai Thongniran <inid3a@gmail.com>
2021-03-24 22:04:48 +0700

    refactor(commit): relocate commit-time-table into module.commit

---
+++ module/commit.py
@@ -0,0 +1,103 @@
+import json
+import urllib.request
+import math
+import sys
+import pytz, dateutil.parser
+
+
+# helper
+def percent_to_progressbar(percent: float):
+    """
+
+    :param percent: percent percentage 53.36
+    :return:
+    """
+    bar_length = 20
+    percent = percent
+
+    n_items = math.floor(percent / 100 * bar_length)
+    n_spaces = bar_length - n_items
+
+    return "[%s%s]" % (n_items * "*", n_spaces * "-")
+
+
+def datetime_from_utc_to_local(date_str: str):
+    """
+
+    :param date_str:
+    :return:
+    """
+
+    global tz
+
+    utctime = dateutil.parser.parse(date_str)
+    localtime = utctime.astimezone(pytz.timezone(tz))
+
+    return localtime
+
+
+# const
+tz = "Asia/Bangkok"
+
+# get data and convert to Dict[]
+target_api = "https://api.github.com/users/jojoee/events?per_page=100"
+res_bytes = urllib.request.urlopen(target_api).read()
+res_str = res_bytes.decode("utf-8")
+events = json.loads(res_str)
+
+# proceed
+event_type_d = {}
+clock_count_d = {
+    "morning": 0,
+    "day": 0,
+    "evening": 0,
+    "night": 0,
+}
+for event in events:
+    # count event types
+    event_type = event["type"]
+    if event_type not in event_type_d:
+        event_type_d[event_type] = 0
+    event_type_d[event_type] = event_type_d[event_type] + 1
+
+    # count commit event
+    if event_type == "PushEvent":
+        # e.g. '2020-10-02T10:05:24Z'
+        local_date = datetime_from_utc_to_local(event['created_at'])
+        hr_and_min = float(local_date.strftime('%H.%M'))  # only hr and min
+
+        if hr_and_min > 18:
+            clock_count_d["evening"] = clock_count_d["evening"] + 1
+        elif hr_and_min > 12:
+            clock_count_d["day"] = clock_count_d["day"] + 1
+        elif hr_and_min > 6:
+            clock_count_d["morning"] = clock_count_d["morning"] + 1
+        elif hr_and_min > 0:
+            clock_count_d["night"] = clock_count_d["night"] + 1
+        else:
+            sys.exit("something went wrong")
+
+n_push_events = event_type_d["PushEvent"]
+clock_percent_d = {
+    "morning": round(clock_count_d["morning"] / n_push_events, 2) * 100,
+    "day": round(clock_count_d["day"] / n_push_events, 2) * 100,
+    "evening": round(clock_count_d["evening"] / n_push_events, 2) * 100,
+    "night": round(clock_count_d["night"] / n_push_events, 2) * 100,
+}
+
+
+def show_commit_text() -> None:
+    print("""In the latest %s commits :bug:, am I morning person ? 
+| | | | |%%|
+| --- | --- | --- | --- | --- |
+| :sunny: | Morning | (06.00-12.00] | %s | %.2f |
+| :satisfied: | Daytime | (12.00-18.00] | %s | %.2f |
+| :moon: | Evening | (18.00-00.00] | %s | %.2f |
+| :sleeping: | Night | (00.00-06.00] | %s | %.2f |
+""" % (
+        n_push_events,
+        percent_to_progressbar(clock_percent_d['morning']), clock_percent_d['morning'],
+        percent_to_progressbar(clock_percent_d['day']), clock_percent_d['day'],
+        percent_to_progressbar(clock_percent_d['evening']), clock_percent_d['evening'],
+        percent_to_progressbar(clock_percent_d['night']), clock_percent_d['night'],
+    ))

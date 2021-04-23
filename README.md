3fb87ca3235d695d7aa0df72b6fd23e03845d9ba
Nathachai Thongniran <inid3a@gmail.com>
2021-04-23 22:22:44 +0700

    feat(bot): add new bots: duckbot002, duckbot003

--- README.md
+++ README.md
@@ -9,26 +9,28 @@ In the latest 36 commits :bug:, am I morning person ?
 
 My crypto trading [duckbot](https://github.com/jojoee/duckbot) performance on [ftx.com](https://ftx.com/#a=13144711)
 ```
-  606.47  ┤                     ╭╮
-  566.08  ┤                     │╰──╮╭──
-  525.68  ┤                     │   ╰╯
-  485.29  ┤                     │╮
-  444.90  ┤                     ││   ╭─╮
-  404.51  ┤                     │╰───╯ ╰
-  364.11  ┤           ╭─────────╯
-  323.72  ┤     ╭─────╯───╯    ││
-  283.33  ┤─────╯     │        ││
-  242.94  ┤           │        ││
-  202.54  ┤       ╭───╯        ╰╯
-  162.15  ┤  ╭╮╭──╯
-  121.76  ┼──╯╰╯
-   81.36  ┤
+  629.52  ┤───────╮
+  613.34  ┼╮      │
+  597.17  ┤───────│
+  580.99  ┤ │     │
+  564.81  ┤ │   ╭─│
+  548.64  ┤ ╰───╯ │
+  532.46  ┤       │
+  516.28  ┤       │
+  500.11  ┤       │
+  483.93  ┤       │
+  467.75  ┤       │
+  451.58  ┤       │
+  435.40  ┤       │
+  419.22  ┤       │
+  403.04  ┤       │
+  386.87  ┤       │
+  370.69  ┤       ╰
+  354.51  ┤
 1 tick = 1 day
-latest datetime (UTC+0): 2021-04-23
-latest USD ($): 409.9332
-latest total USD (BNB + USD) ($): 554.4112
-in last 30 records,
-  max: 256.9280, min: 606.4696
-  mean: 377.4219, std: 114.3345
-``` 
+datetime (UTC+0): 2021-04-23
+duckbot001 total usd: 555.3720$ (grid trading / KZM)
+duckbot002 total usd: 529.8660$ (rebalance BNBBULL/USD 50:50)
+duckbot003 total usd: 354.5146$ (rebalance ADABULL/USD 50:50)
+```
 
--- module/duckbot.py
+++ module/duckbot.py
@@ -5,45 +5,58 @@ import os
 import ccxt
 import asciichartpy
 from typing import List
+from dataclasses import dataclass
+
+
+@dataclass
+class FtxBot:
+    sub_account: str
+    api_key: str
+    api_secret: str
+
+
+def get_total_usd(sub_account: str, api_key: str, api_secret: str) -> float:
+    # get balance
+    ftx = ccxt.ftx({"apiKey": api_key, "secret": api_secret, "enableRateLimit": True})
+    ftx.headers = {"FTX-SUBACCOUNT": sub_account} if len(sub_account) > 0 else {}
+    balance = ftx.fetch_balance()
+
+    # total usd
+    total_usd = np.sum([coin['usdValue'] for coin in balance['info']['result']])
+
+    return total_usd
+
 
 # setup
-API_KEY = os.environ.get('API_KEY')
-API_SECRET = os.environ.get('API_SECRET')
-SUB_ACCOUNT = os.environ.get('SUB_ACCOUNT')
+ftx_bots = [
+    FtxBot(
+        os.environ.get('SUB_ACCOUNT'),
+        os.environ.get('API_KEY'),
+        os.environ.get('API_SECRET'),
+    ),
+    FtxBot(
+        os.environ.get('SUB_ACCOUNT2'),
+        os.environ.get('API_KEY2'),
+        os.environ.get('API_SECRET2'),
+    ),
+    FtxBot(
+        os.environ.get('SUB_ACCOUNT3'),
+        os.environ.get('API_KEY3'),
+        os.environ.get('API_SECRET3'),
+    )
+]
+bot_names = [bot.sub_account for bot in ftx_bots]
 df_path = "./module/duckbot.csv"
 
-# get and append into csv file then save
-retry = 3
-usd = -1
-total_usd = -1
-coins: List[str] = []
-for i in range(0, retry):
-    try:
-        # get balance
-        ftx = ccxt.ftx({"apiKey": API_KEY, "secret": API_SECRET, "enableRateLimit": True})
-        ftx.headers = {"FTX-SUBACCOUNT": SUB_ACCOUNT} if len(SUB_ACCOUNT) > 0 else {}
-        balance = ftx.fetch_balance()
+# get data
+total_usds = [get_total_usd(bot.sub_account, bot.api_key, bot.api_secret) for bot in ftx_bots]
 
-        # usd
-        usd = balance['USD']['total']
-
-        # total usd
-        total_usd = np.sum([coin['usdValue'] for coin in balance['info']['result']])
-
-        # coin list
-        coins = [result['coin'] for result in balance['info']['result']]
-
-        break
-    except Exception as e:
-        # unused
-        error_message = "can not get total usd from FTX API, cause of %s" % str(e)
-
+# format
 now_utc = datetime.now(timezone.utc)
 df = pd.read_csv(df_path)
-df.loc[df.shape[0]] = [now_utc, usd, total_usd]
+df.loc[df.shape[0]] = [now_utc] + total_usds  # append to the last
 df.to_csv(df_path, index=False)
-# fill empty with previous value
-df['total_usd'] = df['total_usd'].replace(to_replace=-1, method='ffill')
+
 # plot to ascii graph
 df['utc_date'] = df['utc_datetime'].map(lambda s: str(s)[0:10])  # create date column
 df = df.groupby('utc_date').nth(0).reset_index()  # group and pick first
@@ -53,34 +66,26 @@ df = df.tail(30)  # only last 30 days
 def show_duckbot_text() -> None:
     global df
 
-    total_usds = df['total_usd'].tolist()
-    usds = df['usd'].tolist()
     dates = df['utc_date'].tolist()
-    data = [usds, total_usds]
+    data = [df["%s_total_usd" % name].tolist() for name in bot_names]
     flatten_data = np.array(data).flatten()
     print("""My crypto trading [duckbot](https://github.com/jojoee/duckbot) performance on [ftx.com](https://ftx.com/#a=13144711)
 ```
 %s
 1 tick = 1 day
-latest datetime (UTC+0): %s
-latest USD ($): %.4f
-latest total USD (%s) ($): %.4f
-in last 30 records,
-  max: %.4f, min: %.4f
-  mean: %.4f, std: %.4f
-``` 
+datetime (UTC+0): %s
+duckbot001 total usd: %.4f$ (grid trading / KZM)
+duckbot002 total usd: %.4f$ (rebalance BNBBULL/USD 50:50)
+duckbot003 total usd: %.4f$ (rebalance ADABULL/USD 50:50)
+```
 """ % (
         asciichartpy.plot(data, cfg={
             "min": min(flatten_data),
             "max": max(flatten_data),
-            "height": 12,
+            "height": 16,
         }),
         dates[-1],
-        usds[-1],
-        ' + '.join(map(str, coins)),
-        total_usds[-1],
-        np.min(total_usds),
-        np.max(total_usds),
-        np.average(total_usds),
-        np.std(total_usds),
+        df["duckbot001_total_usd"].tolist()[-1],
+        df["duckbot002_total_usd"].tolist()[-1],
+        df["duckbot003_total_usd"].tolist()[-1]
     ))

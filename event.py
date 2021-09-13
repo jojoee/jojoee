4c13a441605834cce395e233f0cfb72e4267f9a9
Nathachai Thongniran <inid3a@gmail.com>
2021-09-13 15:40:50 +0700

    fix(github): add query string (/api/utcnow?refresh) to an image api

--- event.py
+++ event.py
@@ -1,6 +1,6 @@
 from module.commit import show_commit_text
 from module.duckbot import show_duckbot_text
 
-print("""Hi :smiley: :wave: <img src="https://jojoee.jojoee.com/api/utcnow" width="120" height="20">""")
+print("""Hi :smiley: :wave: <img src="https://jojoee.jojoee.com/api/utcnow?refresh" width="120" height="20">""")
 show_commit_text()
 show_duckbot_text()

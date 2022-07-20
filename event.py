c78b96c2568626041667cf9bd2e08ed54edef97e
Nathachai Thongniran <inid3a@gmail.com>
2022-07-20 22:22:43 +0700

    chore(hack): refresh the image cache by changing query string

--- event.py
+++ event.py
@@ -1,4 +1,4 @@
 from module.commit import show_commit_text
 
 show_commit_text()
-print("![Current UTC time](https://jojoee.jojoee.com/api/utcnowgif?refresh)")
+print("![Current UTC time](https://jojoee.jojoee.com/api/utcnowgif?now)")
